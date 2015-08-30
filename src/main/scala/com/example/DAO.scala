fb959f9c1633073dbe5839bc161f5d487b174e87
folex <0xdxdy@gmail.com>
2015-08-30 15:10:36 +0300

    Tree has been built!

--- src/main/scala/com/example/DAO.scala
+++ src/main/scala/com/example/DAO.scala
@@ -1,6 +1,7 @@
 package com.example
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
 
 object DAO {
 
@@ -16,8 +17,10 @@ object DAO {
     GlobalSettings.loggingSQLAndTime = GlobalSettings.loggingSQLAndTime.copy(enabled = true, singleLineMode = true, warningEnabled = true)
 
     sql"""PRAGMA foreign_keys = on""".execute().apply()
-    //    sql"""drop table if exists executionLines""".execute().apply()
-    //    sql"""drop table if exists executions""".execute().apply()
+    if (Main.shouldParse) {
+      sql"""drop table if exists executionLines""".execute().apply()
+      sql"""drop table if exists executions""".execute().apply()
+    }
     sql"""create table if not exists executions(trace_id TEXT PRIMARY KEY, start_time INTEGER, end_time INTEGER)""".execute().apply()
     sql"""create table if not exists executionLines(id INT PRIMARY KEY, trace_id STRING, line TEXT, FOREIGN KEY (trace_id) REFERENCES executions(trace_id))""".execute().apply()
   }
@@ -57,27 +60,51 @@ object DAO {
   object ExecutionTable extends SQLSyntaxSupport[Execution] {
     override val tableName = "executions"
 
-    private val e = ExecutionTable.syntax("e")
+    private val e = syntax
 
     def all(limit: Option[Int] = None): List[Execution] = withSQL {
       select.all.from(ExecutionTable as e).limit(limit.getOrElse(-1))
-    }.map(wrs => apply(e)(wrs)).list().apply()
+    }.map(wrs => apply(e.resultName)(wrs)).list().apply()
 
-    def apply(s: SyntaxProvider[Execution])(rs: WrappedResultSet): Execution = Execution(rs.get(s.traceId), rs.get(s.startTime), rs.get(s.endTime))
+    def apply(s: SyntaxProvider[Execution])(rs: WrappedResultSet): Execution = {
+      Execution(rs.get(s.traceId), rs.get(s.startTime), rs.get(s.endTime))
+    }
+
+    def apply(s: ResultName[Execution])(rs: WrappedResultSet): Execution = {
+      Execution(rs.get(s.traceId), rs.get(s.startTime), rs.get(s.endTime))
+    }
+
   }
 
-  case class Line(id: Int, traceId: String, line: String)
+  case class Line(traceId: String, line: String)
 
   object LineTable extends SQLSyntaxSupport[Line] {
     override val tableName = "executionLines"
 
-    val l = syntax("l")
+    val l = syntax
     val lc = column
 
-    def getExecutionLines(traceId: String): List[Line] = withSQL {
-      select.from(LineTable as l).where.eq(lc.traceId, traceId)
-    }.map(wrs => apply(l)(wrs)).list().apply()
+    def getExecutionLines(traceId: String): List[Line] = {
+      val q = withSQL {
+        select.from(LineTable as l).where.eq(lc.traceId, traceId)
+      }
 
-    def apply(s: SyntaxProvider[Line])(rs: WrappedResultSet): Line = Line(rs.get(s.id), rs.get(s.traceId), rs.get(s.line))
+      val t = Try(q.map(wrs => apply(l.resultName)(wrs)).list().apply())
+      if (t.isFailure) {
+        print(s"""Executing " ${q.statement} " with traceId $traceId has lead to failure ${t.failed.get}""")
+      }
+      t.get
+    }
+
+    def apply(s: SyntaxProvider[Line])(rs: WrappedResultSet): Line = {
+      val traceId: String = rs.get(s.traceId)
+      val line: String = rs.get(s.line)
+      Line(traceId, line)
+    }
+    def apply(s: ResultName[Line])(rs: WrappedResultSet): Line = {
+      val traceId: String = rs.get(s.traceId)
+      val line: String = rs.get(s.line)
+      Line(traceId, line)
+    }
   }
 }
--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -8,14 +8,18 @@ import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 
 object Main extends scala.App {
+  val shouldParse = false
+
   DAO.init()
 
   import parselog._
 
   val log = LoggerFactory.getLogger("main")
   log.info("Started")
-  //  parse("filtered.log")
-  val trees = getTreeFromDAO(Some(2))
+  if(shouldParse) {
+    parse("small.log")
+  }
+  val trees = getTreeFromDAO(Some(1))
 
   log.info(s"Finished")
   //    val data = makeHistData(parsedLog)
@@ -63,6 +67,7 @@ object parselog {
   def getTreeFromDAO(limit: Option[Int] = None) = {
     val executions = ExecutionTable.all(limit = limit)
     val filledExecutions = executions.map(e => e.copy(lines = ArrayBuffer(e.getLines.map(_.line): _*)))
+    println(s"Executions are \n\t" + filledExecutions.mkString("\n\t"))
     filledExecutions.map(parseExecutionToTree)
   }
 
@@ -77,10 +82,10 @@ object parselog {
         val id = m.group(4).toLong
         val totalTime = m.group(5).toLong
 
-        val startTime = execution.startTime + timeOffset
-        val endTime = startTime + totalTime
+        val endTime = execution.startTime + timeOffset
+        val startTime = endTime - totalTime
 
-        Some(FutureInfo(id, startTime, endTime, futureName, status, totalTime))
+        Some(FutureInfo(id, startTime, endTime, futureName, status, totalTime, timeOffset))
       } else None
     }
 
@@ -90,30 +95,47 @@ object parselog {
     /*
      * We always assume that remainingInfos are sorted by `(fi => (fi.startTime - fi.endTime, fi.endTime))`
      */
-    def buildTree(root: FutureInfo, remainingInfos: List[FutureInfo]): List[FutureNode] = {
+    def buildTree(root: FutureInfo, remainingInfos: List[FutureInfo], level: Int, parent: Int): List[FutureNode] = {
+//      if(level > 431) throw new Exception("Всё пропало")
+      Main.log.info(s"\nLevel $level parent $parent")
+      var j = 0
       @tailrec
       def findNodes(rem: List[FutureInfo], total: Long = 0, neighbors: List[FutureInfo] = List.empty): List[FutureInfo] = {
-        if (total < root.total) {
-          val neighbor = rem.maxBy(fi => (fi.total, -fi.startTime))
-          val newRem = rem.filter(_.startTime >= neighbor.endTime)
-          findNodes(newRem, total + neighbor.total, neighbor +: neighbors)
+        j += 1
+        if (total < root.total && rem.nonEmpty) {
+          val neighbor = rem.minBy(fi => (fi.startTime, -fi.total))
+          val newRem = rem.filter(fi => fi.startTime >= neighbor.endTime && fi.endTime <= root.endTime && fi.id != neighbor.id)
+          findNodes(rem = newRem, total = total + neighbor.total, neighbors = neighbor +: neighbors)
         } else {
           neighbors
         }
       }
 
       val neighbors = findNodes(remainingInfos)
+//      println(s"Level $level parent $parent neighbors.size ${neighbors.size}")
 
       val remaining = remainingInfos.filterNot(fi => neighbors.exists(_.id == fi.id))
+//      println(s"Level $level parent $parent remaining.size ${remaining.size}")
 
-      neighbors.map(n => FutureNode(n, buildTree(n, remaining)))
+      neighbors.zipWithIndex.map { case (n, ni) =>
+//        println(s"Will process neighbor $ni ${n.name} from level $level; parent $parent")
+        val rems = remaining.filter(fi => fi.startTime >= n.startTime && fi.endTime <= n.endTime)
+        val res = FutureNode(n, buildTree(n, rems, level + 1, ni))
+//        println(s"Finished processing $ni ${n.name} from level $level; parent $parent\n")
+        res
+      }
     }
 
-    FutureNode(initial, buildTree(initial, sorted))
+    FutureNode(initial, buildTree(initial, sorted, 0, -1))
   }
+
+  def printTreeToFlame(tree: FutureNode) = {
+
+  }
+
   case class FutureTime(success: Long = 0L, failure: Long = 0L) {
     def +(time: FutureTime) = FutureTime(success + time.success, failure + time.failure)
   }
   case class FutureNode(info: FutureInfo, nodes: List[FutureNode])
-  case class FutureInfo(id: Long, startTime: Long, endTime: Long, name: String, status: String, total: Long)
+  case class FutureInfo(id: Long, startTime: Long, endTime: Long, name: String, status: String, total: Long, timeOffset: Long)
 }
